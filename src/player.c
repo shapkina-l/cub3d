98f60617b6daf64296c4d0251a98b430190a9ea0
lshapkin <lshapkin@student.42.fr>
2025-07-25 11:59:08 +0200

    norm

--- src/player.c
+++ src/player.c
@@ -3,8 +3,8 @@
 
 int	wall_check(double move_speed, t_game *game, int code)
 {
-	double x;
-	double y;
+	double	x;
+	double	y;
 
 	if (code == 1)
 	{
@@ -29,64 +29,63 @@ int	wall_check(double move_speed, t_game *game, int code)
 	return (game->map->map[(int)y][(int)x] != '1');
 }
 
-void    move_player(t_game *game)
+void	move_player(t_game *game, t_player *player)
 {
-    double move_speed;
+	double	move_speed;
 
-    move_speed = 0.1;
-    if (game->player->move_forward == 1 && wall_check(move_speed, game, 1))
+	move_speed = 0.1;
+	if (player->move_forward == 1 && wall_check(move_speed, game, 1))
 	{
-		game->player->x += game->player->dir_x * move_speed;
-		game->player->y += game->player->dir_y * move_speed;
+		player->x += player->dir_x * move_speed;
+		player->y += player->dir_y * move_speed;
 	}
-	if (game->player->move_backward == 1 && wall_check(move_speed, game, 2))
+	if (player->move_backward == 1 && wall_check(move_speed, game, 2))
 	{
-		game->player->x -= game->player->dir_x * move_speed;
-		game->player->y -= game->player->dir_y * move_speed;
+		player->x -= player->dir_x * move_speed;
+		player->y -= player->dir_y * move_speed;
 	}
-	if (game->player->move_right == 1 && wall_check(move_speed, game, 3))
+	if (player->move_right == 1 && wall_check(move_speed, game, 3))
 	{
-		game->player->x -= game->player->dir_y * move_speed;
-		game->player->y += game->player->dir_x * move_speed;
+		player->x -= player->dir_y * move_speed;
+		player->y += player->dir_x * move_speed;
 	}
-	if (game->player->move_left == 1 && wall_check(move_speed, game, 4))
+	if (player->move_left == 1 && wall_check(move_speed, game, 4))
 	{
-		game->player->x += game->player->dir_y * move_speed;
-		game->player->y -= game->player->dir_x * move_speed;
+		player->x += player->dir_y * move_speed;
+		player->y -= player->dir_x * move_speed;
 	}
 }
 
-void    rotate_player(t_game *game)
+void	rotate_player(t_player *player, t_raycasting *rc)
 {
-    double rot_speed;
-    double old_dir_x;
-    double old_plane_x;
+	double	speed;
+	double	old_dir_x;
+	double	old_plane_x;
 
-	rot_speed = 0.05;
-    if (game->player->rotate_right == 1)
+	speed = 0.05;
+	if (player->rotate_right == 1)
 	{
-		old_dir_x = game->player->dir_x;
-		game->player->dir_x = game->player->dir_x * cos(rot_speed) - game->player->dir_y * sin(rot_speed);
-		game->player->dir_y = old_dir_x * sin(rot_speed) + game->player->dir_y * cos(rot_speed);
-	
-		old_plane_x = game->raycast->plane_x;
-		game->raycast->plane_x = game->raycast->plane_x * cos(rot_speed) - game->raycast->plane_y * sin(rot_speed);
-		game->raycast->plane_y = old_plane_x * sin(rot_speed) + game->raycast->plane_y * cos(rot_speed);
+		old_dir_x = player->dir_x;
+		player->dir_x = player->dir_x * cos(speed) - player->dir_y * sin(speed);
+		player->dir_y = old_dir_x * sin(speed) + player->dir_y * cos(speed);
+		old_plane_x = rc->plane_x;
+		rc->plane_x = rc->plane_x * cos(speed) - rc->plane_y * sin(speed);
+		rc->plane_y = old_plane_x * sin(speed) + rc->plane_y * cos(speed);
 	}
-	if (game->player->rotate_left == 1)
+	if (player->rotate_left == 1)
 	{
-		old_dir_x = game->player->dir_x;
-		game->player->dir_x = game->player->dir_x * cos(-rot_speed) - game->player->dir_y * sin(-rot_speed);
-		game->player->dir_y = old_dir_x * sin(-rot_speed) + game->player->dir_y * cos(-rot_speed);
-
-		old_plane_x = game->raycast->plane_x;
-		game->raycast->plane_x = game->raycast->plane_x * cos(-rot_speed) - game->raycast->plane_y * sin(-rot_speed);
-		game->raycast->plane_y = old_plane_x * sin(-rot_speed) + game->raycast->plane_y * cos(-rot_speed);
+		old_dir_x = player->dir_x;
+		player->dir_x = player->dir_x * cos(-speed)
+			- player->dir_y * sin(-speed);
+		player->dir_y = old_dir_x * sin(-speed) + player->dir_y * cos(-speed);
+		old_plane_x = rc->plane_x;
+		rc->plane_x = rc->plane_x * cos(-speed) - rc->plane_y * sin(-speed);
+		rc->plane_y = old_plane_x * sin(-speed) + rc->plane_y * cos(-speed);
 	}
 }
 
-void update_player(t_game *game)
+void	update_player(t_game *game)
 {
-	move_player(game);
-	rotate_player(game);
+	move_player(game, game->player);
+	rotate_player(game->player, game->raycast);
 }
